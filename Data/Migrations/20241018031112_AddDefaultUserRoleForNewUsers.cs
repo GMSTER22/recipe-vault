993c210b0736ac13634fe8cd0476a9749e15caf4
anthonb711 <anthonb711@byui.edu>
2024-10-17 23:21:38 -0400

    Updating userRole migration for default Role as user and Added Auth to Counter Page

---
+++ Data/Migrations/20241018031112_AddDefaultUserRoleForNewUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeVault.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultUserRoleForNewUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoleId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
